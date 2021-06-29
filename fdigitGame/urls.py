b94a88515c4a3879e8b31f070070a44c0fe47be1
albl2008 <alvaroblanco10@gmail.com>
2021-06-29 20:15:54 -0300

    Mejoras en prueba de numero

--- fdigitGame/urls.py
+++ fdigitGame/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.add, name="add"),
+    path('',views.generar, name="generar")
 ]
--- fdigitGame/views.py
+++ fdigitGame/views.py
@@ -2,6 +2,7 @@ from typing import NewType
 from django.shortcuts import render
 from .models import Newnumber
 from django.contrib import messages
+from .generateNumber import genNum
 
 # Create your views here.
 def add(request):
@@ -11,6 +12,17 @@ def add(request):
 
     if request.method=='POST':
         number = request.POST['number']
-        Newnumber.objects.create(number=number)
-        messages.success(request,'Numero agregado')
+        if len(set(number)) == 4:
+            Newnumber.objects.create(number=number)
+            messages.success(request,'Numero agregado')
+        elif len(set(number)) != 4 :
+            messages.warning(request,'Numero sin repeticiones y/o 4 cifras')
+
+
     return render(request,'fdigitGame/nuevoNumero.html',{'numbers':numbers})
+
+def generar():
+    lista = [1,2,3,4,5,6,7,8,9,0]
+    all = genNum(lista)
+    guess = all.generatingNumber()
+    return render('fdigitGame/nuevoNumero.html',{'guess':guess})
